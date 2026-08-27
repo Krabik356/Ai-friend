0ce24383559adad3369c9b3ef23c6bb1b1e0eb7e
Krabik356 <bereza3622@gmail.com>
2026-08-27 14:26:33 +0300

    Невеличка оптимізація у main server, перейменування імені моделі у ai server

--- main server/main.py
+++ main server/main.py
@@ -36,12 +36,19 @@ def get_user_id(req: Request):
         raise HTTPException(status_code=400, detail="incorrect usage of endpoint")
     return user_id
 
-def is_valid_id(req: Request):
+async def is_valid_id(req: Request):
     user_id = req.headers.get("id", False)
     if user_id == False:
         raise HTTPException(status_code=400, detail="incorrect usage of endpoint")
     if user_id not in audios.keys():
         raise HTTPException(status_code=403, detail="unknown id")
+    async with pool.connection() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute("SELECT EXISTS(SELECT id from users WHERE id=%s)", (user_id,))
+            is_exists = (await cur.fetchone())[0]
+            print(is_exists)
+            if not is_exists:
+                raise HTTPException(status_code=403, detail="unknown id")
     return user_id
 
 async def get_conn():
@@ -64,8 +71,12 @@ def start(user_id=Depends(get_user_id)):
 async def record(req: Request, user_id=Depends(is_valid_id)):
     audios[user_id].extend(await req.body())
 
-async def ai_response(user_data, audio, messages, conn, user_id):
+async def ai_response(user_data, audio, messages, user_id):
     async with app.state.http.stream("POST", "/ai/generate/stream/voice", json={"reputation": user_data["reputation"], "user_audio": base64.b64encode(audio).decode("ascii"), "messages": [item for message in sorted(messages, key=lambda msg: msg["id"]) for item in ({"role": "user", "content": message["user_req"]}, {"role": "assistant", "content": message["ai_resp_text"]})]}) as resp:
+
+        if resp.status_code not in range(200, 300):
+            raise HTTPException(status_code=500, detail="server`s error")
+
         ai_resp_text = ""
         user_req = ""
         reputation = ""
@@ -80,26 +91,29 @@ async def ai_response(user_data, audio, messages, conn, user_id):
             reputation = data_json["reputation"]
             yield base64.b64decode(data_json["ai_resp_voice"])
 
-        async with conn.cursor() as cur:
-            await cur.execute("INSERT INTO messages(user_id, user_req, ai_resp_text) VALUES(%s, %s, %s)", (user_id, user_req, ai_resp_text))
-            await cur.execute("UPDATE users SET reputation=%s WHERE id=%s", (reputation, user_id))
+        async with pool.connection() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute("INSERT INTO messages(user_id, user_req, ai_resp_text) VALUES(%s, %s, %s)", (user_id, user_req, ai_resp_text))
+                await cur.execute("UPDATE users SET reputation=%s WHERE id=%s", (reputation, user_id))
 
 @app.get("/audio/stop", status_code=200)
-async def stop(user_id=Depends(is_valid_id), conn=Depends(get_conn)):
-    audio = audios[user_id]
+async def stop(user_id=Depends(is_valid_id)):
+    audio = audios.pop(user_id)
 
     messages = []
     user_data = []
-    async with conn.cursor(row_factory=dict_row) as cur:
-        await cur.execute("SELECT id, user_req, ai_resp_text FROM messages WHERE user_id=%s ORDER BY id DESC LIMIT 100", (user_id,))
-        messages = await cur.fetchall()
+    async with pool.connection() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(
+                "SELECT id, user_req, ai_resp_text FROM messages WHERE user_id=%s ORDER BY id DESC LIMIT 100",
+                (user_id,))
+            messages = await cur.fetchall()
 
-        await cur.execute("SELECT reputation FROM users WHERE id=%s ORDER BY id ASC", (user_id,))
-        user_data = await cur.fetchone()
-
+            await cur.execute("SELECT reputation FROM users WHERE id=%s", (user_id,))
+            user_data = await cur.fetchone()
 
     return StreamingResponse(
-        ai_response(user_data, audio, messages, conn, user_id),
+        ai_response(user_data, audio, messages, user_id),
         media_type="application/octet-stream"
     )
 
