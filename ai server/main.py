defaa944cf428e8987c2cb94feeb3518dd71e284
Krabik356 <bereza3622@gmail.com>
2026-08-27 12:21:45 +0300

    Робочий прототип обох серверів. Потребує оптимізації, роботи з виключеннями.

--- ai server/main.py
+++ ai server/main.py
@@ -4,31 +4,102 @@ import ollama
 import whisper
 from piper import PiperVoice
 import wave
+import base64
+import json
 
 
 app = FastAPI()
 voice_to_text = whisper.load_model(name="medium", device="cuda")
 text_to_voice = PiperVoice.load(model_path="uk_UA-oleksa-high.onnx", config_path="uk_UA-oleksa-high.onnx.json", use_cuda=True)
+ratings = {
+    "friendly": {
+        "description": "Дуже дружнє спілкування з використанням компліментів",
+        "value": 10,
+    },
+    "interesting": {
+        "description": "Спілкування на цікаві/незвичні теми",
+        "value": 5,
+    },
+    "normal": {
+        "description": "Звичайне, буденне спілкування",
+        "value": 1,
+    },
+    "sad": {
+        "description": "Спілкування на сумні теми, легкі підколи",
+        "value": -3,
+    },
+    "rude": {
+        "description": "Жорстоке спілкування, можливо, з використанням нецензурних слів",
+        "value": -10,
+    },
+}
+deviders = [".", "?", "!"]
 
-async def generate_generator(text):
-    ollama_resp = ollama.chat()
+
+async def generate_generator(reputation, messages):
+    ollama_resp = ollama.chat(model="gemma4:e2b", messages=messages, stream=True, think=False)
+    text = ""
+    for chunk in ollama_resp:
+        text += chunk.message.content
+        if any(devider in text for devider in deviders):
+            voice_bytes = bytearray()
+            for voice_part in text_to_voice.synthesize(text):
+                voice_bytes.extend(voice_part.audio_int16_bytes)
+            yield json.dumps({"reputation": reputation, "ai_resp_text": text, "ai_resp_voice": base64.b64encode(voice_bytes).decode("ascii"), "user_req": messages[len(messages)-1]})+"\n"
+            print(text)
+            text = ""
 
 @app.post("/ai/generate/stream/voice", status_code=200)
 async def generate(req: Request):
-    audio = await req.body()
+    print("start")
+    data = await req.json()
+    reputation = data["reputation"]
+    messages = data["messages"]
+    user_audio = base64.b64decode(data["user_audio"])
 
     with wave.open("temp.wav", "wb") as file:
         file.setframerate(16000)
         file.setsampwidth(2)
         file.setnchannels(1)
-        file.writeframes(audio)
+        file.writeframes(user_audio)
 
     whisper_resp = voice_to_text.transcribe(audio="temp.wav", language="uk")
     user_text = whisper_resp["text"]
+    messages.append({"role": "user", "content": user_text})
+
+    rating = ""
+    for i in range(3):
+        ollama_rep = ollama.generate(model="gemma4:e2b", system=f'Обери в залежності від настрою цього повідомлення оцінку для нього серед: {", ".join(ratings.keys())}. Не пиши нічного окрім одного з наданих тобі слів, інакше код зламається. При цьому, {", ".join(f'{key} - {value["description"]}' for key, value in ratings.items())}. Також не пиши ніякого форматування, навіть Markdown. Приклад №1: користувач: "Привіт, як справи?", ти: "normal". Приклад №2: користувач: "Привіт, розкажи будь-ласка, як працює штучний інтелект?", ти: "interesting"', prompt=messages[len(messages) - 1]["content"], think=False)
+        rating = ollama_rep.response
+        if rating in ratings.keys():
+            break
+        if rating not in ratings.keys() and i == 2:
+            rating = "normal"
+
+    reputation += ratings[rating]["value"]
+    if reputation < 0:
+        reputation = 0
+    elif reputation > 100:
+        reputation = 100
+
+    system = ""
+    if reputation in range(50, 70 + 1):
+        system = "Ти дружня нейромережа-хлопчик. Тебе звати Квен. Ти відповідаєш на запитання користувача, не використовуєш ніякого форматування і навіть Markdown. "
+    elif reputation in range(70, 90 + 1):
+        system = "Ти дружня нейромережа-хлопчик. Тебе звати Квен. Ти відповідаєш на запитання користувача вічливо, з зацікавленістю, не використовуєш ніякого форматування і навіть Markdown. "
+    elif reputation in range(90, 100 + 1):
+        system = "Ти надзвичайно дружня нейромережа-хлопчик. Тебе звати Квен. Ти відповідаєш на запитання користувача вічливо, з зацікавленістю, задаєш зустрічні запитання, не використовуєш ніякого форматування і навіть Markdown. "
+    elif reputation in range(40, 50 + 1):
+        system = "Ти нейромережа-хлопчик. Тебе звати Квен. Ти відповідаєш на запитання користувача трохи прохолодно, не використовуєш ніякого форматування і навіть Markdown. "
+    elif reputation in range(0, 40 + 1):
+        system = "Ти нейромережа-хлопчик. Тебе звати Квен. Ти відповідаєш на запитання користувача дуже холодно, коротко, ти ображений на користувача, не використовуєш ніякого форматування і навіть Markdown. "
+    system += f'Використовуй лише ці роздільники тексту: {", ".join(deviders)}'
+
+    messages.insert(0, {"role": "system", "content": system})
 
     return StreamingResponse(
-        generate_generator(user_text),
-        media_type="application/octet-stream"
+        generate_generator(reputation, messages),
+        media_type="application/x-ndjson"
     )
 
 
--- main server/main.py
+++ main server/main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, Depends
+from fastapi.responses import StreamingResponse
+from psycopg.errors import UniqueViolation
 from psycopg_pool import AsyncConnectionPool
+from psycopg.rows import dict_row
 from contextlib import asynccontextmanager
+import httpx
+import base64
+import json
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await pool.open()
-    yield
-    await pool.close()
+    async with httpx.AsyncClient(base_url="http://192.168.0.103:8001", timeout=60) as http:
+        app.state.http = http
+
+        await pool.open()
+        yield
+        await pool.close()
 
 app = FastAPI(lifespan=lifespan)
 pool = AsyncConnectionPool(
@@ -15,6 +25,83 @@ pool = AsyncConnectionPool(
     min_size=2,
     open=False
 )
+
+
+audios = {}
+
+
+def get_user_id(req: Request):
+    user_id = req.headers.get("id", False)
+    if user_id == False:
+        raise HTTPException(status_code=400, detail="incorrect usage of endpoint")
+    return user_id
+
+def is_valid_id(req: Request):
+    user_id = req.headers.get("id", False)
+    if user_id == False:
+        raise HTTPException(status_code=400, detail="incorrect usage of endpoint")
+    if user_id not in audios.keys():
+        raise HTTPException(status_code=403, detail="unknown id")
+    return user_id
+
+async def get_conn():
+    async with pool.connection() as conn:
+        yield conn
+
+@app.get("/user/register", status_code=201)
+async def register_user(user_id=Depends(get_user_id), conn=Depends(get_conn)):
+    async with conn.cursor() as cur:
+        try:
+            await cur.execute("INSERT INTO users(id, reputation) VALUES(%s, %s)", (user_id, 50))
+        except UniqueViolation:
+            raise HTTPException(status_code=409, detail="user already exists")
+
+@app.get("/audio/start", status_code=201)
+def start(user_id=Depends(get_user_id)):
+    audios[user_id] = bytearray()
+
+@app.post("/audio/record", status_code=200)
+async def record(req: Request, user_id=Depends(is_valid_id)):
+    audios[user_id].extend(await req.body())
+
+async def ai_response(user_data, audio, messages, conn, user_id):
+    async with app.state.http.stream("POST", "/ai/generate/stream/voice", json={"reputation": user_data["reputation"], "user_audio": base64.b64encode(audio).decode("ascii"), "messages": [item for message in sorted(messages, key=lambda msg: msg["id"]) for item in ({"role": "user", "content": message["user_req"]}, {"role": "assistant", "content": message["ai_resp_text"]})]}) as resp:
+        ai_resp_text = ""
+        user_req = ""
+        reputation = ""
+        async for data in resp.aiter_lines():
+
+            if not data:
+                continue
+
+            data_json = json.loads(data)
+            ai_resp_text += data_json["ai_resp_text"]
+            user_req += data_json["user_req"]["content"]
+            reputation = data_json["reputation"]
+            yield base64.b64decode(data_json["ai_resp_voice"])
+
+        async with conn.cursor() as cur:
+            await cur.execute("INSERT INTO messages(user_id, user_req, ai_resp_text) VALUES(%s, %s, %s)", (user_id, user_req, ai_resp_text))
+            await cur.execute("UPDATE users SET reputation=%s WHERE id=%s", (reputation, user_id))
+
+@app.get("/audio/stop", status_code=200)
+async def stop(user_id=Depends(is_valid_id), conn=Depends(get_conn)):
+    audio = audios[user_id]
+
+    messages = []
+    user_data = []
+    async with conn.cursor(row_factory=dict_row) as cur:
+        await cur.execute("SELECT id, user_req, ai_resp_text FROM messages WHERE user_id=%s ORDER BY id DESC LIMIT 100", (user_id,))
+        messages = await cur.fetchall()
+
+        await cur.execute("SELECT reputation FROM users WHERE id=%s ORDER BY id ASC", (user_id,))
+        user_data = await cur.fetchone()
+
+
+    return StreamingResponse(
+        ai_response(user_data, audio, messages, conn, user_id),
+        media_type="application/octet-stream"
+    )
 
 
 
